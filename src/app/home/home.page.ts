203e6dd70b32ef1dbbffb3f98cf3e910d410f5bc
Sonia-Baibou <s.baibou@ohmycode.io>
2023-12-21 13:41:03 +0100

    add ios - issue map

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, Platform } from '@ionic/angular/standalone';
+import { GoogleMap } from '@capacitor/google-maps';
+
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,60 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePage {
-  constructor() {}
+  @ViewChild('map') mapRef!: ElementRef<HTMLElement>;
+  //@Input() datas!: any[];
+  private api_key: string = 'AIzaSyCPLwGXnUjTxgSk60GIqoSXI4YpvVLocYE';
+  private newMap!: GoogleMap;
+  constructor(private platform: Platform) {}
+
+  // ngOnInit() {
+
+  // }
+
+
+
+  async createMap() {
+    // on crée une carte
+    //console.log(this.mapRef.nativeElement, 'native');
+
+    console.log('testAPIKEY');
+    // create a new map
+    this.newMap = await GoogleMap.create({
+      id: 'my-map',
+      element: this.mapRef.nativeElement,
+      apiKey: 'AIzaSyCPLwGXnUjTxgSk60GIqoSXI4YpvVLocYE',
+      config: {
+        center: {
+          lat: 48.992128,
+          lng: 2.2779189,
+        },
+        zoom: 12,
+      },
+    });
+    console.log(this.newMap, 'newMap');
+  }
+
+  ionViewDidEnter() {
+    this.platform.ready().then(() => {
+      console.log('platform is ready');
+
+      if (this.platform.is('ios')) {
+        console.log('map inside ios');
+        //this.api_key = environment.GOOGLE_MAP_API_KEY_IOS;
+        this.createMap();
+      } else if (this.platform.is('android')) {
+        console.log('map inside android');
+        //this.api_key = environment.GOOGLE_MAP_API_KEY_ANDROID;
+        this.createMap();
+      } else {
+        console.log('inside else');
+        //this.api_key = environment.GOOGLE_MAP_TEST;
+        console.log(this.api_key);
+        this.createMap();
+      }
+    });
+  }
 }
